053271b4fa3e999c81bd04d4e22819e08d1bc238
Anna Jachym <ania.jachym@gmail.com>
2018-10-16 21:53:53 +0200

    commit

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
  */
  // we check if the browser supports Service Worker
 if ("serviceWorker" in navigator){
+  window.addEventListener("load", function(){
+
 // if it does then we register Service Wroker
-  navigator.serviceWorker.register("./js/sw.js")
-  .then(function(reg){
-    console.log("It worked", reg)
+  navigator.serviceWorker.register("sw.js", {scope:"./"})
+  .then(function(registration){
+    console.log("It worked with scope: ", registration.scope)
  // if not then registration fails
   }).catch(function(error){
     console.log("Service Worker failed", error);
   });
+});
 }
