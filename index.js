2851b953cbb21830ab224f15f1eb17fad3746c68
Anna Jachym <ania.jachym@gmail.com>
2018-10-22 14:44:59 +0200

    added responsive

--- index.js
+++ index.js
@@ -4,11 +4,10 @@
  // we check if the browser supports Service Worker
 if ("serviceWorker" in navigator){
 // if it does then we register Service Wroker
-  navigator.serviceWorker.register("./sw.js")
-  .then(function(registration){
+  navigator.serviceWorker.register("sw.js").then(function(registration){
     console.log("It worked with scope: "+ registration.scope);
  // if not then registration fails
   }).catch(function(error){
     console.log("Service Worker failed", error);
-  });
+  })
 };
